4c6a6fe32eccb13b039d174e140bccce1dbc39b3
Sasikumar Sunkara <84332179+Sunkarasasikumar@users.noreply.github.com>
2022-08-25 22:47:51 +0530

    def is_prime(p):     if p<2:         return False     for i in range(2,(p+2)//2):         if p%i==0:             return False     return True N=int(input()) l=list(map(int,input().split())) k=int(input()) count=0 for i in range(len(l)):     if is_prime(l[i]) and l[i]<=k:         count+=1 print(count)

---
+++ primes_less_than_k.py
@@ -0,0 +1,15 @@
+def is_prime(p):
+    if p<2:
+        return False
+    for i in range(2,(p+2)//2):
+        if p%i==0:
+            return False
+    return True
+N=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+count=0
+for i in range(len(l)):
+    if is_prime(l[i]) and l[i]<=k:
+        count+=1
+print(count)
