bd0bdaeb138822c201521691a37683d2775f499d
Sasikumar Sunkara <84332179+Sunkarasasikumar@users.noreply.github.com>
2022-08-26 01:37:37 +0530

    def desc(a):     for i in range(len(a)-1):         if a[i]<a[i+1]:             return False     return True n=int(input()) l=list(map(int,input().split())) print("yes" if desc(l) else "no")

---
+++ Descending_array.py
@@ -0,0 +1,8 @@
+def desc(a):
+    for i in range(len(a)-1):
+        if a[i]<a[i+1]:
+            return False
+    return True
+n=int(input())
+l=list(map(int,input().split()))
+print("yes" if desc(l) else "no")
