f42e9b61d09b62d580ceeb086d76e2f7cc6ded9b
Sasikumar Sunkara <84332179+Sunkarasasikumar@users.noreply.github.com>
2022-08-26 23:40:22 +0530

    n=input() n=n.lower() l=n.split() vowel=['a','e','i','o','u'] mcount=[] for i in l:     count=0     for j in i:         if j in vowel:             count+=1     mcount.append(count) mcount.sort() print(mcount[0])

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,15 @@
+n=input()
+n=n.lower()
+l=n.split()
+vowel=['a','e','i','o','u']
+mcount=[]
+for i in l:
+    count=0
+    for j in i:
+        if j in vowel:
+            count+=1
+    mcount.append(count)
+mcount.sort()
+print(mcount[0])
+
+    
