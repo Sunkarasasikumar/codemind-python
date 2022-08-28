90a8e5d3ce1e019892a2e4af0c48c8638e9cae16
Sasikumar Sunkara <84332179+Sunkarasasikumar@users.noreply.github.com>
2022-08-28 20:37:06 +0530

    n=int(input()) l=list(map(int,input().split())) count=0 min=max=-1 for i in range(n):     if l[i]%2==0:         min=i         break for i in range(n-1,-1,-1):     if l[i]%2==0:         max=i         break for i in range(min+1,max):     if l[i]%2==0:         count+=1 print(count)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=list(map(int,input().split()))
+count=0
+min=max=-1
+for i in range(n):
+    if l[i]%2==0:
+        min=i
+        break
+for i in range(n-1,-1,-1):
+    if l[i]%2==0:
+        max=i
+        break
+for i in range(min+1,max):
+    if l[i]%2==0:
+        count+=1
+print(count)
